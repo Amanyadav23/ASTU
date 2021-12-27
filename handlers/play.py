fe80950aaca35c5768fc26c41c3558ba4281b017
Amanyadav23 <88925295+Amanyadav23@users.noreply.github.com>
2021-12-28 00:53:25 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -82,7 +82,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
     draw.text(
         (190, 670),
-        f"Powered By: S4Shiv(@Shivamdemon)",
+        f"Powered By: 𝘼𝙎𝙏𝙐(@Astu_back)",
         (255, 255, 255),
         font=font,
     )
